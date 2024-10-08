9992b3e6cc782ef69666e8133cd978a7862895bf
rax1mjon <tulaganovraximjon65@gmail.com>
2024-10-08 12:28:29 +0500

    new dark mode

--- js/main.js
+++ js/main.js
@@ -50,3 +50,16 @@ function shrink() {
 // function topFunction() {
 //   window.scrollTo(0, 0);
 // }
+
+
+// dark-mode
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "./images/light.svg";
+  } else {
+    this.firstElementChild.src = "./images/moon.svg";
+  }
+  document.body.classList.toggle("dark");
+});
